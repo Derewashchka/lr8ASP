e8e7a312cea5077a08c2051db3187bd28ca8ddd2
My comp <My comp@DESKTOP-HBDEDEI>
2024-11-23 12:34:57 +0200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using lr8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lr8.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { ID = 1, Name = "Laptop", Price = 1500, CreatedDate = DateTime.Now },
+                new Product { ID = 2, Name = "Smartphone", Price = 800, CreatedDate = DateTime.Now.AddDays(-1) },
+                new Product { ID = 3, Name = "Tablet", Price = 300, CreatedDate = DateTime.Now.AddDays(-5) }
+            };
+
+            return View(products);
+        }
+    }
+}
